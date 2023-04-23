dfed99038b18c3b4e21140192a574f0330636524
Joseph Yu <39754176+joseph082@users.noreply.github.com>
2023-04-23 00:39:26 -0700

    Add duplicate classes test

--- tester.py
+++ tester.py
@@ -124,7 +124,8 @@ def generate_test_suite_v1():
         1,
         ["test_inputi", "test_recursion1", "test_set_field",  # passes
          "test_hello_world", "test_inputs"],  # added cases
-        ["test_if", "test_incompat_operands1"],  # fails
+        ["test_if", "test_incompat_operands1",  # fails
+         "test_duplicate_classes"], # added cases
     )
 
 
